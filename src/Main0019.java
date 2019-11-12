2382e0dcff4f3c157d3cc12e62f3e0e8193ba7e7
lzbluering <2435300020@qq.com>
2019-11-12 15:02:17 +0800

    2019.11.12-当天练习结果【涉及：双指针（辅助指针遍历）】

---
+++ src/Main0019.java
@@ -0,0 +1,64 @@
+/**
+ * 删除链表的倒数第N个节点
+ *
+ * 给定一个链表，删除链表的倒数第?n?个节点，并且返回链表的头结点。
+ *
+ * 示例：
+ *
+ * 给定一个链表: 1->2->3->4->5, 和 n = 2.
+ *
+ * 当删除了倒数第二个节点后，链表变为 1->2->3->5.
+ * 说明：
+ *
+ * 给定的 n?保证是有效的。
+ *
+ * 进阶：
+ *
+ * 你能尝试使用一趟扫描实现吗？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-nth-node-from-end-of-list
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main0019 {
+
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if(head == null || head.next == null){
+            return null;
+        }
+        ListNode p = head;
+
+        for(int i=0; i<n-1; i++){
+            p = p.next;
+        }
+        if(p.next == null){
+            return head.next;
+        }
+        ListNode pNext = head;
+        p = p.next;
+        while(p.next!=null){
+            p=p.next;
+            pNext=pNext.next;
+        }
+
+        pNext.next = pNext.next.next;
+
+        return head;
+    }
+
+}
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; }
+}
+/**
+ * 解题思路：
+ * 1、双指针，第一个指针 p 先遍历 n，此时如果 p.next==null 则表示删除节点在正数第一个,否则 p=p.next
+ * 2、第二个指针 pNext 与 p 一起遍历，当 p.next==null 则表示 pNext.next 是要删除节点
+ *
+ * 极端情况：删除第一个节点
+ */
