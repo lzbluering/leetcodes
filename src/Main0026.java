cd88e95260d7c0dfd8c23e4c7ad9e07a3257077a
lzbluering <2435300020@qq.com>
2019-11-14 15:45:36 +0800

    2019.11.14-当天练习结果【涉及双指针】

---
+++ src/Main0026.java
@@ -0,0 +1,51 @@
+/**
+ * 删除排序数组中的重复项
+ *
+ * 给定一个排序数组，你需要在原地删除重复出现的元素，使得每个元素只出现一次，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 示例?1:
+ *
+ * 给定数组 nums = [1,1,2],
+ *
+ * 函数应该返回新的长度 2, 并且原数组 nums 的前两个元素被修改为 1, 2。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例?2:
+ *
+ * 给定 nums = [0,0,1,1,1,2,2,3,3,4],
+ *
+ * 函数应该返回新的长度 5, 并且原数组 nums 的前五个元素被修改为 0, 1, 2, 3, 4。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-array
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main0026 {
+
+    public int removeDuplicates(int[] nums) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int index = 1;
+        int result = 1;
+        for(int i=1; i<nums.length; ++i){
+            if(nums[i-1] != nums[i]){
+                nums[index++] = nums[i];
+                ++result;
+            }
+        }
+
+        return result;
+    }
+
+}
+/**
+ * 解题思路：
+ * 双指针
+ * 利用双指针：一个遍历数组、一个记录数组不重复的记录位置
+ */
