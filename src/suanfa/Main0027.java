468fbd31d32887455d68197df2b8903909208e1c
lizhens <lizhens@yonyou.com>
2019-11-16 18:38:06 +0800

    11.16-提交【涉及：位移、KMP算法】

--- src/suanfa/Main0027.java
+++ src/suanfa/Main0027.java
@@ -1,36 +1,50 @@
 package suanfa;
 
 /**
- * 实现?strStr()?函数。
+ * 移除元素
  *
- * 给定一个?haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回? -1。
+ * 给定一个数组 nums?和一个值 val，你需要原地移除所有数值等于?val?的元素，返回移除后数组的新长度。
+ *
+ * 不要使用额外的数组空间，你必须在原地修改输入数组并在使用 O(1) 额外空间的条件下完成。
+ *
+ * 元素的顺序可以改变。你不需要考虑数组中超出新长度后面的元素。
  *
  * 示例 1:
  *
- * 输入: haystack = "hello", needle = "ll"
- * 输出: 2
- * 示例 2:
+ * 给定 nums = [3,2,2,3], val = 3,
  *
- * 输入: haystack = "aaaaa", needle = "bba"
- * 输出: -1
- * 说明:
+ * 函数应该返回新的长度 2, 并且 nums 中的前两个元素均为 2。
  *
- * 当?needle?是空字符串时，我们应当返回什么值呢？这是一个在面试中很好的问题。
+ * 你不需要考虑数组中超出新长度后面的元素。
+ * 示例?2:
  *
- * 对于本题而言，当?needle?是空字符串时我们应当返回 0 。这与C语言的?strstr()?以及 Java的?indexOf()?定义相符。
+ * 给定 nums = [0,1,2,2,3,0,4,2], val = 2,
+ *
+ * 函数应该返回新的长度 5, 并且 nums 中的前五个元素为 0, 1, 3, 0, 4。
+ *
+ * 注意这五个元素可为任意顺序。
+ *
+ * 你不需要考虑数组中超出新长度后面的元素。
  *
  * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/implement-strstr
+ * 链接：https://leetcode-cn.com/problems/remove-element
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class Main0027 {
 
-    public int strStr(String haystack, String needle) {
-        return -1;
+    public int removeElement(int[] nums, int val) {
+        if(nums == null || nums.length == 0){
+            return 0;
+        }
+
+        int index = 0;
+        for(int i=0; i<nums.length; ++i){
+            if(nums[i] != val){
+                nums[index++] = nums[i];
+            }
+        }
+
+        return index;
     }
 
 }
-/**
- * 解题思路：
- * KMP 算法
- */
---
+++ src/suanfa/Main0029.java
@@ -0,0 +1,59 @@
+package suanfa;
+
+/**
+ * 两数相除
+ *
+ * 给定两个整数，被除数?dividend?和除数?divisor。将两数相除，要求不使用乘法、除法和 mod 运算符。
+ *
+ * 返回被除数?dividend?除以除数?divisor?得到的商。
+ *
+ * 示例?1:
+ *
+ * 输入: dividend = 10, divisor = 3
+ * 输出: 3
+ * 示例?2:
+ *
+ * 输入: dividend = 7, divisor = -3
+ * 输出: -2
+ * 说明:
+ *
+ * 被除数和除数均为 32 位有符号整数。
+ * 除数不为?0。
+ * 假设我们的环境只能存储 32 位有符号整数，其数值范围是 [?231,? 231?? 1]。本题中，如果除法结果溢出，则返回 231?? 1。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/divide-two-integers
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class Main0029 {
+
+    public int divide(int dividend, int divisor) {
+        boolean sign = (dividend > 0) ^ (divisor > 0);
+        int result = 0;
+        if(dividend>0) {
+            dividend = -dividend;
+        }
+        if(divisor>0) divisor = -divisor;
+        while(dividend <= divisor) {
+            int temp_result = -1;
+            int temp_divisor = divisor;
+            while(dividend <= (temp_divisor << 1)) {
+                if(temp_divisor <= (Integer.MIN_VALUE >> 1))break;
+                temp_result = temp_result << 1;
+                temp_divisor = temp_divisor << 1;
+            }
+            dividend = dividend - temp_divisor;
+            result += temp_result;
+        }
+        if(!sign) {
+            if(result <= Integer.MIN_VALUE) return Integer.MAX_VALUE;
+            result = - result;
+        }
+        return result;
+    }
+
+}
+/**
+ * 解题思路：
+ * 利用除数左移右移、注意溢出
+ */
